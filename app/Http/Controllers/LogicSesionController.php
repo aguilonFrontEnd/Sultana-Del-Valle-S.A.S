72e94e70be454e51ab224df1a988dca4176bca45
AguilonFrontEnd <aguilondevelopsoft@gmail.com>
2025-11-19 17:13:16 -0500

    TAB003
    Implemetation of the tables Power Bi v3

--- app/Http/Controllers/LogicSesionController.php
+++ app/Http/Controllers/LogicSesionController.php
@@ -162,12 +162,12 @@ class LogicSesionController extends Controller
     ];
 
     $powerbiUrls = [
-        'operativo' => 'https://app.fabric.microsoft.com/reportEmbed?reportId=28c3b66a-1714-4938-9247-2baa8f87ea5b&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
-        'documentacion' => 'https://app.powerbi.com/reportEmbed?reportId=557738c9-9afe-4a93-a934-146ae6c77a7c&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
-        'siniestros' => 'https://app.powerbi.com/reportEmbed?reportId=16fae2c4-6705-4e48-9f96-5cf2b5c23bca&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
-        'mantenimiento' => 'https://app.powerbi.com/reportEmbed?reportId=22f792ef-8b75-4bc6-8b93-96a20145c2e4&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
+        'operativo' => 'https://app.powerbi.com/reportEmbed?reportId=c9aec2c0-56d3-4df5-83f3-c170b09f08ad&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
+        'documentacion' => 'https://app.powerbi.com/reportEmbed?reportId=b8a42692-073a-49e7-95f2-73c99ad2c432&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
+        'siniestros' => 'https://app.powerbi.com/reportEmbed?reportId=20a9942b-09a2-4df8-ad13-b6cfd66caa2d&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
+        'mantenimiento' => 'https://app.powerbi.com/reportEmbed?reportId=2a26394d-6c84-4d9c-b396-5257114a9ff1&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
         'analistas' => 'https://app.powerbi.com/reportEmbed?reportId=bc3e7363-4d20-42d5-8fdb-9813be575122&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
-        'humanidad' => 'https://app.powerbi.com/reportEmbed?reportId=5370504f-1ab5-4370-a7b2-c6e36c695b3b&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
+        'humanidad' => 'https://app.powerbi.com/reportEmbed?reportId=cbadc814-1a0d-42cc-bc73-21176b2e2590&autoAuth=true&ctid=bfcae52b-3054-486c-998b-518ff055dcaa',
         'liquidacion' => '#',
         'cartera' => '#'
     ];
